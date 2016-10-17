42900466b7ce4dcad10bb94443c169081a378bf3
jdaly04 <julia.m.daly@gmail.com>
2016-10-16 21:31:32 -0400

    added cardCreate logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,9 +2,19 @@ var cardOne = 'queen';
 var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
+var cards = ('queen', 'king', 'queen', 'king');
+//if (cardTwo === cardFour || cardOne === cardTwo || cardOne === cardThree || cardOne === cardFour || cardTwo === cardThree || cardThree === cardFour) {
+//  alert ('You found a match!')
+//} else {
+  //alert ('Sorry, try again!')
+//}
+var board = document.getElementById('board-game');
 
-if (cardTwo === cardFour || cardOne === cardTwo || cardOne === cardThree || cardOne === cardFour || cardTwo === cardThree || cardThree === cardFour) {
-  alert ('You found a match!')
-} else {
-  alert ('Sorry, try again!')
+function createCard() {
+  for (var i=0; i<cards.length; i++) {
+    var makeCardElement = document.createElement('div');
+makeCardElement.className = 'card';
+board.appendChild(makeCardElement);
 }
+}
+createCard(4);
